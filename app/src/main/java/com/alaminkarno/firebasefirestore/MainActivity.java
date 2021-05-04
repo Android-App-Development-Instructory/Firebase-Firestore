638a379653131de943e31f5fb266eb01a4ecefe3
User <https://github.com/karno786/Sqlite_git_test>
2021-05-05 02:22:53 +0600

    Upload and View Image 2

--- app/src/main/java/com/alaminkarno/firebasefirestore/MainActivity.java
+++ app/src/main/java/com/alaminkarno/firebasefirestore/MainActivity.java
@@ -77,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
         }).addOnFailureListener(new OnFailureListener() {
             @Override
             public void onFailure(@NonNull Exception e) {
-                Toast.makeText(MainActivity.this, "Error: "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "Error SMS: "+e.getMessage(), Toast.LENGTH_SHORT).show();
             }
         }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
             @Override
