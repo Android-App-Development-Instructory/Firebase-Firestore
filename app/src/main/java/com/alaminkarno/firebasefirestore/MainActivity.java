e93aebf8fa465156cb27490d747612800bbe8f60
User <https://github.com/karno786/Sqlite_git_test>
2021-05-05 02:20:44 +0600

    Upload and View Image

---
+++ app/src/main/java/com/alaminkarno/firebasefirestore/MainActivity.java
@@ -0,0 +1,91 @@
+package com.alaminkarno.firebasefirestore;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView,viewImage;
+    private static int REQUEST_CODE = 1;
+    private Uri imageUri;
+    private String imageDownloadLink;
+    private StorageReference storageReference;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        init();
+    }
+
+    private void init() {
+        imageView = findViewById(R.id.imageView);
+        viewImage = findViewById(R.id.viewImage);
+        storageReference = FirebaseStorage.getInstance().getReference();
+    }
+
+    public void chooseImage(View view) {
+        Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        intent.setType("image/*");
+        startActivityForResult(intent,REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(resultCode == RESULT_OK && requestCode == REQUEST_CODE){
+            imageUri = data.getData();
+            if(imageUri != null){
+                imageView.setImageURI(imageUri);
+            }
+        }
+    }
+
+
+    public void saveImage(View view) {
+        StorageReference imageRef = storageReference.child("image"+ UUID.randomUUID());
+
+        imageRef.putFile(imageUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(MainActivity.this, "Image Upload Successfully.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(MainActivity.this, "Error: "+e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                imageDownloadLink = imageUri.toString();
+
+                Picasso.get().load(imageDownloadLink).into(viewImage);
+            }
+        });
+    }
+}
